31cfd1e82227969d0bcd245304eab10a7aae7b71
Gwangmin Choi <crazy4u5@naver.com>
2022-04-14 02:08:28 +0900

    dataset made

---
+++ convert_to_dataset.py
@@ -0,0 +1,59 @@
+import logging as log
+import pandas as pd
+import numpy as np
+
+log.basicConfig(level=log.DEBUG)
+
+WEATHER_DATA = "./results/weather.csv"
+DATALAB_DATA = "./results/datalab_norm_list.csv"
+
+def merge_datasets():
+    df_weather = pd.DataFrame(pd.read_csv(WEATHER_DATA))
+    df_datalab = pd.DataFrame(pd.read_csv(DATALAB_DATA))
+    df_datalab_ranked = convert_dataset_by_value(df_datalab)
+    df_results = multiply_rows(df_weather, df_datalab_ranked)
+    
+    log.debug(df_results.head(10))
+    df_results.to_csv("./results/model_dataset.csv")
+
+def convert_dataset_by_value(df : pd.DataFrame):
+    result = pd.DataFrame(columns=["date", "0", "1", "2", "3", "4"])
+    for row in df.itertuples():
+        res_row = {"date" : row[2]}
+        comp_dict = row._asdict()
+        del comp_dict["Index"]
+        del comp_dict["_1"]
+        del comp_dict["기간"]
+        for i in range(5):
+            max = 0.0
+            max_key = None
+            for k, v in comp_dict.items():
+                if max < v:
+                    max = v
+                    max_key = k
+            res_row[str(i)] = max_key
+            del comp_dict[max_key]
+        result.loc[len(result)] = res_row
+    result = result.set_axis(["date", "first", "second", "third", "fourth", "fifth"], axis=1)
+    return result
+
+def multiply_rows(df : pd.DataFrame, ranked_df : pd.DataFrame):
+    number_to_repeats = {"first" : 5, "second" : 4, "third" : 3, "fourth" : 2, "fifth" : 1}
+    result = pd.DataFrame(columns=["date", "precipitation", "avg_temp", "lowest_temp", "highest_temp", "is_rain", "is_snow", "is_holiday", "meal_type"])
+    for row_wea, row_rank in zip(df.itertuples(), ranked_df.itertuples()):
+        dict_rank = row_rank._asdict()
+        del dict_rank["Index"]
+        del dict_rank["date"]
+        for k, v in dict_rank.items():
+            weather = row_wea._asdict()
+            del weather["_1"]
+            weather["meal_type"] = v
+            for _ in range(number_to_repeats[k]):
+                result.loc[len(result)] = weather
+    return result
+
+def main():
+    merge_datasets()
+
+if __name__ == '__main__':
+    main()
---
+++ weatherdatasetconvert.py
@@ -0,0 +1,53 @@
+import string
+import pandas as pd
+
+def convert_dataset(percfilename : string, tempfilename : string):
+    with open("./holidays.txt", "r", encoding="utf-8") as f:
+        holidays = [str(x).replace("\n", "") for x in f.readlines()]
+
+    df_perc = pd.DataFrame(pd.read_csv(percfilename).drop(columns=["지점"]))
+    df_temp = pd.DataFrame(pd.read_csv(tempfilename).drop(columns=["지점"]))
+
+    df = df_perc.merge(df_temp, how="outer", on="날짜")
+    df = df.set_axis(["date", "precipitation", "avg_temp", "lowest_temp", "highest_temp"], axis=1)
+
+    df["precipitation"] = pd.to_numeric(df["precipitation"])
+    df["avg_temp"] = pd.to_numeric(df["avg_temp"])
+    df["lowest_temp"] = pd.to_numeric(df["lowest_temp"])
+    df["highest_temp"] = pd.to_numeric(df["highest_temp"])
+    df["is_rain"] = df.apply(lambda x : is_rain(x["precipitation"], x["avg_temp"]), axis=1)
+    df["is_snow"] = df.apply(lambda x : is_snow(x["precipitation"], x["avg_temp"]), axis=1)
+    df["is_holiday"] = df.apply(lambda x : is_holiday(x["date"], holidays), axis=1)
+    df.to_csv("./results/weather.csv")
+
+def is_rain(prec, temp):
+    if prec > 0.0 and temp >= 0.0:
+        return True
+    else:
+        return False
+
+def is_snow(prec, temp):
+    if prec > 0.0 and temp < 0.0:
+        return True
+    else:
+        return False
+
+def is_minus(temp : float):
+    if (temp >= 0):
+        return True
+    else:
+        return False
+
+def is_holiday(date, holidays : list):
+    dayname = pd.to_datetime(date).day_name()
+    if date in holidays or dayname == "Saturday" or dayname == "Sunday":
+        return True
+    else:
+        return False
+
+def main():
+    convert_dataset("weather_perc.csv", "weather_temp.csv")
+
+if __name__ == '__main__':
+    main()
+    
